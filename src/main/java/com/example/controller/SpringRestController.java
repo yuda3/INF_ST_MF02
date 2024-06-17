2fe4771dba83ed6f6bd6e887376efdfce4d7642d
yuda3 <yuda3@hotmail.com>
2024-06-17 18:48:36 +0900

    restApi　追加

--- src/main/java/com/example/controller/SpringRestController.java
+++ src/main/java/com/example/controller/SpringRestController.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import com.example.entity.Book;
+import com.example.repository.BookMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,8 @@ import java.util.List;
 
 @RestController
 public class SpringRestController {
+    @Autowired
+    BookMapper mapper;
     @RequestMapping("/rest")
     public List<String> rest(){
         List<String> list = new ArrayList<>();
@@ -15,6 +20,12 @@ public class SpringRestController {
         list.add("do");
         list.add("well");
         return list;
-
     }
+
+    @RequestMapping("/restlist")
+    public List<Book> list(){
+        List<Book> list = mapper.bookList();
+        return list;
+    }
+
 }
